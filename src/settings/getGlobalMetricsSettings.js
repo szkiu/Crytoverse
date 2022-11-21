6d4620fb4988ca79913169a1d89da3f0da520cad
sZKiu <augustomendez544@gmail.com>
2022-11-21 10:05:37 -0300

    For see all page, install a plugin to avoid the cors

--- src/settings/getGlobalMetricsSettings.js
+++ src/settings/getGlobalMetricsSettings.js
@@ -1,4 +1,4 @@
 const URL_API_KEY = "?CMC_PRO_API_KEY=ec152bca-3221-4e8c-b03c-8219b02b59de";
-export const URL_GLOBALMETRICS = `global-metrics/quotes/latest${URL_API_KEY}`;
+export const URL_GLOBALMETRICS = `https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest${URL_API_KEY}`;
 
 // https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?CMC_PRO_API_KEY=ec152bca-3221-4e8c-b03c-8219b02b59de
